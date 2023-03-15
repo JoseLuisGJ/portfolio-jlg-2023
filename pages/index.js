4f81b4c0022b8d61123ec12352f41f8b25f6d6b2
José Luis González <joseluisgj@gmail.com>
2023-03-15 16:46:35 +0100

    home responsive layout improvements

--- pages/index.js
+++ pages/index.js
@@ -34,15 +34,10 @@ export default function Home() {
 
   const animateUI = () => {
     // Sets
-    gsap.set(h1SpanRef.current, {
-      y: 0
-    });
     gsap.set(backgroundRef.current, {
       filter: "brightness(1)",
     });
-    gsap.set(h1Ref.current, {
-      x: -200
-    });
+
     gsap.set(glitchBoyRef.current, {
       filter: "blur(0px) brightness(1)",
       y: 0,
@@ -51,8 +46,10 @@ export default function Home() {
     //   opacity: 0
     // });
     // Init animations
-    gsap.from(h1Ref.current, { duration: 0.8, opacity: 0, x: -90, delay: 0.7 });
-    gsap.from(h2Ref.current, { duration: 0.8, opacity: 0, x: 120, delay: 1.2 });
+
+    // gsap.from(h1Ref.current, { duration: 0.8, opacity: 0, x: -90, delay: 0.7 });
+    // gsap.from(h2Ref.current, { duration: 0.8, opacity: 0, x: 120, delay: 1.2 });
+
     // gsap.to(arrowRef.current, { duration: 1.5, opacity: 1, delay: 2 });
     // Scroll animations
     gsap.to(glitchBoyRef.current, {
@@ -76,16 +73,16 @@ export default function Home() {
         markers: false
       }
     });
-    gsap.to(h1SpanRef.current, { 
-      y: -210,
-      scrollTrigger: {
-        trigger: h1SpanRef.current, 
-        start: "top center",
-        end: "top top",
-        scrub: true,
-        markers: false
-      }
-    });
+    // gsap.to(h1SpanRef.current, { 
+    //   y: -210,
+    //   scrollTrigger: {
+    //     trigger: h1SpanRef.current, 
+    //     start: "top center",
+    //     end: "top top",
+    //     scrub: true,
+    //     markers: false
+    //   }
+    // });
     gsap.to(arrowRef.current, { 
       opacity: 0,
       scrollTrigger: {
@@ -123,30 +120,30 @@ export default function Home() {
       
 
         <div className={styles.main}>
-          <h1 ref={h1Ref}  className='text-6xl font-thin' >
-            <span className='block' ref={h1SpanRef}> Hello! I&apos;m{" "}
-              <span className='font-normal' id="text-shuffle">José Luis González </span></span>
+          <h1 ref={h1Ref}  className='absolute top-8 md:top-[35%] left-6 md:left-[40%] md:-translate-x-56 z-[3] text-5xl md:text-5xl lg:text-6xl font-thin' >
+            <span className='block' ref={h1SpanRef}> Hello! I&apos;m{" "}<br/>
+              <span className='font-normal' id="text-shuffle">José Luis<br/>González </span></span>
           </h1>
           <div className=' absolute z-[5] h-screen w-screen'>
           </div>
           <div ref={glitchBoyRef} className={`${styles.glitchBoyRef} fixed z-[2] h-screen w-screen`}>
             <GlitchBoy />
           </div>
-          <h2 ref={h2Ref}  className='text-3xl font-thin'>Product designer & Code lover
+          <h2 ref={h2Ref} className='absolute bottom-6 md:top-[70%] left-6 md:left-[50%] md:translate-x-24 lg:translate-x-32 text-2xl lg:text-3xl font-thin z-[2] md:z-[1]'>Product<br/>designer &<br/> Code lover
           </h2>
           <a ref={arrowRef} href='#about' className='absolute z-[8] bottom-5 left-1/2 transform -translate-x-1/2' ><Image className='' src="/assets/down-arrow.svg" width={31} height={31} alt="Down arrow" /></a>
         </div>
-        <div id="about" className='about absolute h-screen w-screen flex z-[6] justify-center'>
-          <div className='flex justify-center flex-col content-center center max-w-4xl'>
-            <h1 className='text-center text-6xl py-5 reveal'>About me</h1>
-            <h3 className='text-center text-3xl font-extralight py-5 reveal'>I&apos;ve <b>20 years</b> of professional experience, based in <b>Valencia</b>. Nowadays I work as a <b>Senior UX Designer/Engineer</b> at <a href='http://www.elastic.cco' target="_blank" rel="noreferrer">Elastic</a>, making delightful develover tools.</h3>
-            <p className='text-center text-xl py-5 font-extralight reveal'>Formerly <a href='http://www.newrelic.com' target="_blank" rel="noreferrer">New Relic</a>, <a href='http://www.qatium.com' target="_blank" rel="noreferrer">Qatium</a>, <a href='http://www.idrica.com' target="_blank" rel="noreferrer">Idrica</a>, <a href='http://www.globalomnium.com' target="_blank" rel="noreferrer">Global Omnium</a>, <a href='http://www.nectarestudio.com' target="_blank" rel="noreferrer">Nectar Estudio</a> among others.</p>
-            <div className="grid grid-cols-2 gap-4 text-lg font-extralight py-5 reveal">
+        <div id="about" className='about absolute h-auto min-h-screen w-screen flex z-[6] justify-center'>
+          <div className='flex justify-center flex-col content-center center max-w-4xl p-8'>
+            <h1 className='text-center text-4xl lg:text-6xl py-2 md:py-5 reveal'>About me</h1>
+            <h3 className='text-center text-2xl lg:text-3xl font-extralight py-5 reveal'>I&apos;ve <b>20 years</b> of professional experience, based in <b>Valencia</b>. Nowadays I work as a <b>Senior UX Designer/Engineer</b> at <a href='http://www.elastic.cco' target="_blank" rel="noreferrer">Elastic</a>, making delightful develover tools.</h3>
+            <p className='text-center  text-lg lg:text-xl py-5 font-extralight reveal'>Formerly <a href='http://www.newrelic.com' target="_blank" rel="noreferrer">New Relic</a>, <a href='http://www.qatium.com' target="_blank" rel="noreferrer">Qatium</a>, <a href='http://www.idrica.com' target="_blank" rel="noreferrer">Idrica</a>, <a href='http://www.globalomnium.com' target="_blank" rel="noreferrer">Global Omnium</a>, <a href='http://www.nectarestudio.com' target="_blank" rel="noreferrer">Nectar Estudio</a> among others.</p>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 text-base lg:text-lg font-extralight py-5 reveal">
               <div>I cofounded and worked at <a href='http://www.nectarestudio.com' target="_blank" rel="noreferrer">Nectar Estudio</a> during 8 years. Since then I was collaborating with agencies and companies like <a href='https://www.serviceplan.es/' target="_blank" rel="noreferrer">Publips</a>, <a href='https://marketing4ecommerce.net/la-historica-agencia-shackleton-pasa-a-ser-accenture-song/' target="_blank" rel="noreferrer">Shackleton Digital</a>, <a href='http://www.avantio.com' target="_blank" rel="noreferrer">Avantio</a>, <a href='http://www.energysystem.com' target="_blank" rel="noreferrer">Energy System</a>. Obteining some awards and recognitions by <a href='https://www.awwwards.com/' target="_blank" rel="noreferrer">AWWWARDS</a>, <a href='https://www.thefwa.com/' target="_blank" rel="noreferrer">FWA</a>, <a href='https://premiosadcv.com/' target="_blank" rel="noreferrer">ADCV</a>, <a href='https://www.adg-fad.org/' target="_blank" rel="noreferrer">Laus</a> and <a href='https://festivallalluna.com/' target="_blank" rel="noreferrer">La Lluna</a>.</div>
               <div>Teaching was also and important part of my career. I led the <a href='https://www.esat.es/estudios/postgrado-especializado-en-diseno-y-desarrollo-web-front-end/' target="_blank" rel="noreferrer">ID3 post degree</a> in <a href='http://www.esat.es' target="_blank" rel="noreferrer">ESAT</a>. Where I also taught UX//UI and web frontend development in the New Media Arts 2.0 degree during 12 years. Sharing knowledgement with the future generations.</div>
             </div>
             <div>
-              <ul className="flex justify-center py-5">
+              <ul className="flex justify-center py-5 ">
                 <li className="mr-6">
                   <a href='mailto:hola@joseluis.design' target="_blank" rel="noreferrer">
                     <Image className='' src="/assets/email-icon.svg" width={29} height={21} alt="email" />
