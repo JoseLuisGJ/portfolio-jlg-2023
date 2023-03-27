fae947cf4cc5023892a9454f081f8ef261a1bd98
ergum <joseluisgj@gmail.com>
2023-03-27 17:46:55 +0200

    Elastic instrumentation in addition to New Relic

--- elastic-apm-node.js
+++ elastic-apm-node.js
@@ -2,7 +2,8 @@ module.exports = {
     // Not sure about the service name and secretToken
     // https://www.elastic.co/guide/en/apm/guide/8.6/api-key.html#enable-api-key
     // https://www.elastic.co/guide/en/apm/agent/nodejs/current/nextjs.html
+    // https://a588ce6bb2f14b99ab2e19204eb54157.us-central1.gcp.cloud.es.io/app/integrations/edit-integration/elastic-cloud-apm
 
-    serverUrl: 'https://a588ce6bb2f14b99ab2e19204eb54157.us-central1.gcp.cloud.es.io/',  // E.g. https://my-deployment-name.apm.us-west2.gcp.elastic-cloud.com
-    secretToken: 'LTB1Ukk0Y0J2dk5odnVETFJzSXc6aURnRlZVVk1RQTI3Vll2R2RuZUJZUQ===='
+    serverUrl: 'https://ecd4f288aaf14c959b433ecc1a2f1572.apm.us-central1.gcp.cloud.es.io:443/',  // E.g. https://my-deployment-name.apm.us-west2.gcp.elastic-cloud.com
+    secretToken: 'Nn342dhKTIdj3h6ho5'
   }
