8b26a81a4aaa96b05aad7b14b0fdf5c48adbdb3c
ergum <joseluisgj@gmail.com>
2023-03-27 17:10:02 +0200

    Elastic instrumentation attempt

---
+++ elastic-apm-node.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Not sure about the service name and secretToken
+    // https://www.elastic.co/guide/en/apm/guide/8.6/api-key.html#enable-api-key
+    // https://www.elastic.co/guide/en/apm/agent/nodejs/current/nextjs.html
+
+    serverUrl: 'https://a588ce6bb2f14b99ab2e19204eb54157.us-central1.gcp.cloud.es.io/',  // E.g. https://my-deployment-name.apm.us-west2.gcp.elastic-cloud.com
+    secretToken: 'LTB1Ukk0Y0J2dk5odnVETFJzSXc6aURnRlZVVk1RQTI3Vll2R2RuZUJZUQ===='
+  }
