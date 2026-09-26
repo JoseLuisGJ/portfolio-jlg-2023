5c40c68f62f8f61343801e338e0ce39de5c0c453
JoseLuisGJ <joseluisgj@gmail.com>
2026-09-26 08:05:39 +0200

    Arrow scroll also into the projects cover

--- components/projects/projectCover.js
+++ components/projects/projectCover.js
@@ -1,12 +1,18 @@
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import ProjectBlock from "./projectBlock";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function ProjectCover(props) {
 
+  const coverRef = useRef(null);
   const wrapperRef = useRef(null)
   const titleRef = useRef(null);
+  const arrowRef = useRef(null);
+  const arrowContentRef = useRef(null);
   const subtitleRef = useRef(null);
   const roleTitleRef = useRef(null);
   const roleRef = useRef(null);
@@ -16,10 +22,37 @@ export default function ProjectCover(props) {
   const imageRef = useRef(null);
 
   useEffect(() => {
-    animateUI();
-    return () => {
+    const nextSection = coverRef.current?.nextElementSibling;
+    if (nextSection) nextSection.id = "project-next";
 
-    }
+    animateUI();
+
+    gsap.set(arrowContentRef.current, { opacity: 0, y: -50 });
+    const intro = gsap.to(arrowContentRef.current, {
+      duration: 1.5,
+      opacity: 1,
+      y: 0,
+      delay: 1.5,
+    });
+    const fade = nextSection
+      ? gsap.to(arrowRef.current, {
+          autoAlpha: 0,
+          scrollTrigger: {
+            trigger: nextSection,
+            start: "top bottom",
+            end: "+=300",
+            scrub: true,
+            toggleActions: "play none none reverse",
+            markers: false,
+          },
+        })
+      : null;
+
+    return () => {
+      intro.kill();
+      fade?.scrollTrigger?.kill();
+      fade?.kill();
+    };
   }, []);
 
   const animateUI = () => {
@@ -39,7 +72,7 @@ export default function ProjectCover(props) {
   }
 
   return (
-    <div className='relative flex justify-center'>
+    <div ref={coverRef} className='relative flex justify-center'>
       <div ref={wrapperRef} className='relative opacity-0 flex flex-col-reverse md:flex-row h-screen p-8 xl:p-0 justify-center z-[2] max-w-5xl'>
         <div className={`${props.smallHeroImage ? "basis-2/5" : "basis-1/5"} flex flex-col justify-center`}>
           <h1 ref={titleRef}  className='text-4xl font-bold'>{props.title}</h1>
@@ -71,6 +104,20 @@ export default function ProjectCover(props) {
         </div>
       </div>
       <Image priority placeholder="blur" fill className='object-cover z-[1]' src={props.backgroundImage} alt='Project background image' />
+      <a
+        ref={arrowRef}
+        href="#project-next"
+        className="absolute z-[8] bottom-32 md:bottom-6 left-1/2 transform -translate-x-1/2"
+      >
+        <div className="block" ref={arrowContentRef}>
+          <Image
+            src="/assets/down-arrow.svg"
+            width={31}
+            height={31}
+            alt="Down arrow"
+          />
+        </div>
+      </a>
     </div>
   );
 }
